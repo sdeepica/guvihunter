356cf2eae5b5d4cb7bf67561f4bec2e3195a92cf
sdeepica <sdeeps1028@gmail.com>
2017-08-18 15:51:46 +0530

    Update reversewords.java

--- reversewords.java
+++ reversewords.java
@@ -1,8 +1,9 @@
 package hunter;
-
+import java.util.Scanner;
 public class reversewords {
 	public static void main(String[] args) {
-		String s="hello world";
+		Scanner scan=new Scanner(System.in);
+		String s=scan.nextLine();
 		String[] arr=s.split(" ");
 		for(int i=arr.length-1;i>=0;i--) {
 		   System.out.print(arr[i]+" ");
